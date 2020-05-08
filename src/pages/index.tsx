f29b002e4b8ab6766a4fc584a38dda236aee0045
Esther Falayi <esther.falayi@andela.com>
2020-05-08 14:48:28 +0100

    - convert index component to a class component - update typescript config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/SEO"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/projects/">View projects</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/projects/">View projects</Link>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
